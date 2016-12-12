5877aebd2864e14997e54cbbfcf3c10b76904118
MasterOfSomeTrades <ti_grrrrrr@hotmail.com>
2016-12-11 23:18:14 -0500

    Copy files for Communications

---
+++ CEComms/ClassLibrary1/Communications/SendGrid/CEEmail/EmailSender.cs
@@ -0,0 +1,54 @@
+﻿using CommentEverythingCryptography.Encryption;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockiment.Communications.SendGrid.CEEmail {
+    public class EmailSender {
+        public EmailSender() {
+            MaxSend = 20;
+            CharLimit = 100000;
+        }
+
+        public int MaxSend { get; set; }
+        public int CharLimit { get; set; }
+        private int _numberSent = 0;
+
+        public void SendEmail(string subject, string msg) {
+            IEncryptionProvider decryptor = EncryptionProviderFactory.CreateInstance(EncryptionProviderFactory.CryptographyMethod.AES);
+
+            SendGridAPIClient sg = new SendGridAPIClient(decryptor.Decrypt("vZZswAyoKBvDmSIf8Uso4r14Yen35On52P2rybFzhAjQTyFvjUjxtxOtw5EezQVjSl9KJDu1scyVJeYRRUcltYFqud7wkgjftHFyKEkIYyk ="));
+            Email from = new Email(decryptor.Decrypt("U48Ul4Ozn6/k44sqAIL9hjh/YC/hFSi1okBtaKehUAA="));
+            Email to = new Email(decryptor.Decrypt("T97nnD8qgDCMvR2xFTqBI0B+UYBqW7CQrZv2NGowfjA="));
+
+            if (_numberSent < MaxSend) {
+                if (msg.Length <= CharLimit) {
+                    Content content = new Content("text/plain", msg);
+                    Mail mail = new Mail(from, subject, to, content);
+                    sg.client.mail.send.post(requestBody: mail.Get());
+                } else {
+                    subject = "Undeliverable Mail";
+                    Content content = new Content("text/plain", "Email message length greater than maximum number of characters of " + CharLimit.ToString());
+                    Mail mail = new Mail(from, subject, to, content);
+                    sg.client.mail.send.post(requestBody: mail.Get());
+                }
+            } else if (_numberSent == MaxSend) {
+                subject = "Undeliverable Mail";
+                Content content = new Content("text/plain", "Number of email messages sent in specified time period has reached the limit of " + MaxSend.ToString());
+                Mail mail = new Mail(from, subject, to, content);
+                sg.client.mail.send.post(requestBody: mail.Get());
+            }
+            _numberSent++;
+        }
+
+        // TODO: Counter reset as function of this class
+        // TODO: Count message segments in counter (e.g. using message length)
+        public void ResetCounter() {
+            _numberSent = 0;
+        }
+    }
+}
---
+++ CEComms/ClassLibrary1/Communications/Twilio/SMS/TextMessageSender.cs
@@ -0,0 +1,43 @@
+﻿using CommentEverythingCryptography.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Twilio;
+
+namespace Stockiment.Communications.Twilio.SMS {
+    public class TextMessageSender {
+        public TextMessageSender() {
+            MaxSend = 20;
+            CharLimit = 4000;
+        }
+
+        public int MaxSend { get; set; }
+        public int CharLimit { get; set; }
+        private int _numberSent = 0;
+
+        public void SendText(string msg) {
+            IEncryptionProvider decryptor = EncryptionProviderFactory.CreateInstance(EncryptionProviderFactory.CryptographyMethod.AES);
+
+            // Find your Account Sid and Auth Token at twilio.com/user/account
+            TwilioRestClient twilio = new TwilioRestClient(decryptor.Decrypt("PKPBJJDXite9Ccsm1Skfj2J3zq9c0v7cD8fzljHVNZYvKemFKEhyihaNgP/swDvE"), decryptor.Decrypt("pAP9/hFxQ4ruGvZl5naAihLsOyBcU/kzVtyZnZK7GlAJkDfyh5h49c2vKj/NiUsw"));
+
+            if (_numberSent < MaxSend) {
+                if (msg.Length <= CharLimit) {
+                    Message message = twilio.SendMessage(decryptor.Decrypt("TSFhK1EaNhtPPJIx4UjYBA=="), decryptor.Decrypt("7eYiH6SvvlNxcvdmgX4+Yg=="), msg);
+                } else {
+                    Message message = twilio.SendMessage(decryptor.Decrypt("TSFhK1EaNhtPPJIx4UjYBA=="), decryptor.Decrypt("7eYiH6SvvlNxcvdmgX4+Yg=="), "====\nSMS length over character limit/n====");
+                }
+            } else if (_numberSent == MaxSend) {
+                Message message = twilio.SendMessage(decryptor.Decrypt("TSFhK1EaNhtPPJIx4UjYBA=="), decryptor.Decrypt("7eYiH6SvvlNxcvdmgX4+Yg=="), "====\nSMS alerts maxed out for time period/n====");
+            }
+            _numberSent++;
+        }
+
+        // TODO: Counter reset as function of this class
+        // TODO: Count message segments in counter (e.g. using message length)
+        public void ResetCounter() {
+            _numberSent = 0;
+        }
+    }
+}
